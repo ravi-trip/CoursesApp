6cefe8cef8aeed85f76675a785b602599daa8578
ravitripathi <ravitripathi.biz@gmail.com>
2018-05-28 12:59:01 +0530

    placeholder rest service added

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,11 +1,15 @@
-// import { Injectable } from '@angular/core'; // injectable needed for dependency injection in updated versions
-
-// @Injectable({
-//     providedIn: 'root',
-// })
+import { Injectable } from '@angular/core'; // injectable needed for dependency injection in updated versions
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+@Injectable({
+    providedIn: 'root',
+})
 
 export class CoursesService {
+    constructor(public http: Http) {
+        console.log('Data Service connected...');
+    }
     getCourses() {
-        return ['course1', 'course2', 'course3'];
+        return this.http.get('https://jsonplaceholder.typicode.com/posts').map( res => res.json());
     }
 }
